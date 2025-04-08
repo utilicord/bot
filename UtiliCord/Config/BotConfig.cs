78677128b55691f7dcc62ff0972cc0ef5bde91ba
Patrick <80135860+TigerGamer134@users.noreply.github.com>
2025-04-08 19:35:21 -0400

    Basic Implementation (#1)
    * New project
    
    * Bot startup & config file

---
+++ UtiliCord/Config/BotConfig.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtiliCord.Config
+{
+    public class BotConfig
+    {
+        private const string configFolder = "data";
+        private const string configFile = "config.json";
+        public static readonly Config ClientConfig;
+
+        static BotConfig()
+        {
+            if (!Directory.Exists(configFolder))
+            {
+                Directory.CreateDirectory(configFolder);
+            }
+
+            if (!File.Exists(configFolder + "/" + configFile))
+            {
+                ClientConfig = new Config();
+                string json = JsonConvert.SerializeObject(ClientConfig, Formatting.Indented);
+                File.WriteAllText(configFolder + "/" + configFile, json);
+            }
+            else
+            {
+                string json = File.ReadAllText(configFolder + "/" + configFile);
+                ClientConfig = JsonConvert.DeserializeObject<Config>(json);
+            }
+        }
+
+        public struct Config
+        {
+            [JsonProperty("token")]
+            public string Token { get; set; }
+        }
+    }
+}
---
+++ UtiliCord/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace UtiliCord
+{
+    public class Program
+    {
+        static void Main(string[] args)
+            => MainAsync(args).GetAwaiter().GetResult();
+
+        public static async Task MainAsync(string[] _)
+        {
+            Core.Start();
+
+            await Task.Delay(-1);
+        }
+    }
+}
