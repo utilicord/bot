e2e1c11466b4a95a4589bae46091d6c443730ff0
Patrick <tigerg134@tuta.io>
2025-04-07 23:50:22 -0400

    Bot startup & config file

---
+++ UtiliCord/Core.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.WebSocket;
+using UtiliCord.Config;
+
+namespace UtiliCord
+{
+    public class Core
+    {
+        private const string Version = "canary";
+        private static DiscordSocketClient Client { get; set; }
+
+        public static async void Start()
+        {
+            Client = new DiscordSocketClient();
+            Client.Log += Log;
+
+            await Client.LoginAsync(TokenType.Bot, BotConfig.ClientConfig.Token);
+            await Client.StartAsync();
+        }
+
+        private static Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
--- UtiliCord/Program.cs
+++ UtiliCord/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace UtiliCord
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
+            => MainAsync(args).GetAwaiter().GetResult();
+
+        public static async Task MainAsync(string[] _)
         {
-            Console.WriteLine("Hello, World!");
+            Core.Start();
+
+            await Task.Delay(-1);
         }
     }
 }
